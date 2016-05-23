28bb01c54f1b82e287f9ef2f029aa74a8c2388e1
Andrew Wang <azw7@cornell.edu>
2016-05-23 16:13:03 -0700

    Begin directory traversal/markdown building

--- nanosite.py
+++ nanosite.py
@@ -1,3 +1,15 @@
+import markdown
+import os
+from html import escape as escape_HTML
+
+# compile markdown to HTML, returning (html, meta_info) tuple
+def compile_markdown(md_text):
+    md = markdown.Markdown(extensions = ["markdown.extensions.meta"])
+    html = md.convert(md_text)
+    meta = {k: "".join(v) for k, v in md.Meta.items()} \
+           if md.Meta is not None else {}
+    return (html, meta)
+        
 # fetch key, possibly nested thru dot notation
 def ctx_fetch(ctx, key):
     parts = key.split(".", 1)
@@ -5,11 +17,6 @@ def ctx_fetch(ctx, key):
         return ctx[parts[0]]
     else:
         return ctx_fetch(ctx[parts[0]], parts[1])
-
-# escape HTML braces
-def escape_HTML(s):
-    raise "TODO"
-    return s
 
 # fill template according to rules in doc.txt
 def fill_template(tmpl, ctx):
@@ -105,8 +112,9 @@ def fill_template(tmpl, ctx):
                 for_variable = cmd[1]
                 for_collection = ctx_fetch(ctx, cmd[3])
             else:
-                if key[0] == "{" and key[-1] == "}":
-                    out += str(ctx_fetch(ctx, key[1:-1]))
+                if key[0] == "{":
+                    out += str(ctx_fetch(ctx, key[1:]))
+                    _, rest = get_chunk(rest, "}") # get matching close brace
                 else:
                     out += escape_HTML(str(ctx_fetch(ctx, key)))
     if depth_if > 0:
@@ -114,3 +122,54 @@ def fill_template(tmpl, ctx):
     elif depth_if < 0:
         raise Exception("#endif without #if")
     return out
+
+def build_file(top, path, ctx, template_path):
+    root, ext = os.path.splitext(path)
+    if ext.lower() == ".md":
+        html, meta = compile_markdown(open(path, "r").read())
+
+        tmpl = open(template_path, "r").read()
+        ctx["Content"] = html
+        ctx["Meta"] = meta
+        out_html = fill_template(tmpl, ctx)
+
+        relpath = os.path.relpath(root + ".html", top)
+        out_path = os.path.join(ctx["OutputDir"], relpath)
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+        with open(out_path, "w") as f:
+            f.write(out_html)  # TODO
+    elif ext.lower() == ".html+":
+        pass
+    return "I'M A FILE"
+
+def build_dir(top, path, ctx, template_path=None):
+    files = []
+    dirs = []
+    tree = {}
+    for subdir in os.listdir(path):
+        subpath = os.path.join(path, subdir)
+        if os.path.isfile(subpath):
+            files.append((subdir, subpath))
+            # update template path if there is a template in this folder
+            root, ext = os.path.splitext(os.path.basename(subpath))
+            if root.lower() == "template" and ext.lower() == ".tmpl":
+                template_path = subpath
+        elif os.path.isdir(subpath):
+            dirs.append((subdir, subpath))
+
+    aug_ctx = dict(ctx)
+    for short_path, full_path in dirs:
+        path_name = short_path.capitalize()
+        tree[path_name] = build_dir(top, full_path, ctx, template_path)
+        aug_ctx[path_name] = tree[path_name]
+        
+    for short_path, full_path in files:
+        tree[short_path] = build_file(top, full_path, aug_ctx, template_path)
+
+    return tree
+
+def build_site(top, ctx):
+    build_dir(top, top, ctx)
+
+default_meta = {"OutputDir": "testsite/output/", "TemplateDir": "templates/"}
+build_site("testsite/", default_meta)
